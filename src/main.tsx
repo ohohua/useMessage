9827dcb554b6436ef86d5622383c6570db36bb94
ohohua <ohohua@163.com>
2024-12-19 22:50:59 +0800

    ClickToComponent调试

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { ClickToComponent } from 'click-to-react-component'
 
-createRoot(document.getElementById('root')!).render(<App />)
+// ⌥ + 鼠标左键, 点击组件, 查看组件代码
+
+createRoot(document.getElementById('root')!).render(
+  <>
+    <ClickToComponent />
+    <App />
+  </>,
+)
